ae48ac4e6ea673fe90f9b80ce68ace33c6ac826e
Razeenrizmi <IT24300057@my.sliit.lk>
2026-09-24 12:43:21 +0530

    feat: add due dates to tasks (closes #2)

--- src/tasks.js
+++ src/tasks.js
@@ -1,8 +1,8 @@
 // CampusEats task list
  const tasks = [
-    "Design the menu screen" ,
-    "Build the orders API" ,
-    "Add user login",
+    "Design the menu screen before sep 30" ,
+    "Build the orders API before sep 27" ,
+    "Add user login before sep 25" ,
  ];
 
  console.log('CampusEats has ${tasks.length} open tasks ');
